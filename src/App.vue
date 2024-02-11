b76fa688db485b34c435a402f36c689899e71d4d
jobel v. golde <jobelgolde43@gmail.com>
2024-02-12 07:28:51 +0800

    modifying signinComponent

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,93 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <SigninComponent @loginFunction="loginFunction"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+ import SigninComponent from './components/SigninComponent.vue';
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      dataArray: [],
+      username: '',
+      password: '',
+      getLoginData: null
+    }
+  },
+  props: {
+
+  },
+  components:{
+    SigninComponent
+  },
+  methods: {
+    fetchData(){
+      console.log('fetching data')
+      fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res =>{
+        if(res.ok){
+          console.log('fetching data success...')
+        }
+        return res.json()
+      })
+      .then(data =>{
+        this.dataArray = data
+      
+       for(let i = 0; i < data.length; i++){
+        console.log('id: ' + this.dataArray[i].id)
+       }
+        this.dataArray.push({
+    "id": 11,
+    "name": "Jobel",
+    "username": "Jobel v. Golde",
+    "email": "jobelgolde43@gmail.com",
+    "password": "nonong123",
+    "address": {
+      "street": "Kulas Light",
+      "suite": "Apt. 556",
+      "city": "Gwenborough",
+      "zipcode": "92998-3874",
+      "geo": {
+        "lat": "-37.3159",
+        "lng": "81.1496"
+      }
+    },
+    "phone": "1-770-736-8031 x56442",
+    "website": "hildegard.org",
+    "company": {
+      "name": "Romaguera-Crona",
+      "catchPhrase": "Multi-layered client-server neural-net",
+      "bs": "harness real-time e-markets"
+    }
+  })
+      })
+      .catch(error =>{
+        console.log('Error while fetching data ' + error)
+      })
+    },
+    loginFunction(getLoginData){
+     this.getLoginData = getLoginData
+      this.username = getLoginData[0]
+      this.password = getLoginData[0]
+
+      for(let i = 0; i < this.dataArray.length; i++){
+        if(this.dataArray[i].username === this.username && this.dataArray[i].password === this.password){
+        console.log('Login Success')
+      }else{
+        console.log('Failed')
+      }
+      }
+      
+    }
+    
+  },
+  mounted(){
+    this.fetchData()
   }
+ 
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
---
+++ src/components/SigninComponent.vue
@@ -0,0 +1,197 @@
+<template>
+    <div class="main-content container-fluid bg-light ">
+        <div class="circle-bg"></div>
+        <div class="circle-bg2"></div>
+        <div class="circle-bg3"></div>
+
+        <div class="signin-card d-flex flex-column">
+            <div class="header d-flex flex-row justify-content-between align-items-center mb-2 pt-2 ps-3 pe-3">
+                <div class="logo"><h5 class="fs-5 fw-bold text-dark">Java <span class="text-primary">Rice</span></h5></div>
+                <div class="links"><a href="#">About</a></div>
+            </div>
+
+            <div class="body  d-flex flex-row gap-2">
+                <div class="image"></div>
+                <div class="content">
+                <h5 class="text-primary mt-3 ms-2 welcome-back" v-if="!signupIsClicked">Welcome Back!</h5>
+                <h5 class="text-primary mt-3 ms-2 signup" v-if="signupIsClicked">Signup</h5>
+                <form action="#" class="form container-fluid mt-4">
+     
+                    <div class="input-group mb-3" v-if="signupIsClicked">
+                    <span class="input-group-text" id="basic-addon1"><i class="bi bi-envelope"></i></span>
+                    <input type="text" class="form-control" placeholder="Email" aria-label="Username" aria-describedby="basic-addon1">
+                    </div>
+
+            <div class="input-group mb-3">
+            <span class="input-group-text" id="basic-addon1"><i class="bi bi-person"></i></span>
+            <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" v-model="username">
+            </div>
+
+
+            <div class="input-group mb-3">
+            <span class="input-group-text" id="basic-addon1"><i class="bi bi-lock"></i></span>
+            <input type="text" class="form-control" placeholder="Password" aria-label="Username" aria-describedby="basic-addon1" v-model="password">
+            </div>
+
+            <div class="input-group mb-3" v-if="signupIsClicked">
+            <span class="input-group-text" id="basic-addon1"><i class="bi bi-lock"></i></span>
+            <input type="text" class="form-control" placeholder="Confirm password" aria-label="Username" aria-describedby="basic-addon1">
+            </div>
+
+             <div class="button-container">
+                <button class="btn btn-primary">
+                    <span v-if="!signupIsClicked" @click="loginFunc">Login</span>
+                    <span v-if="signupIsClicked">Done</span>
+                </button>
+                <div class="mt-2">
+                   <p v-if="!signupIsClicked"> Didn't have an account? | <a href="#" @click="signupFunc">Signup</a> </p>
+                   <p v-if="signupIsClicked"> Already have an account? | <a href="#" @click="signinFunc">Login</a> </p>
+                </div>
+             </div>
+                </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+ export default{
+    name: 'SigninComponent',
+    data(){
+        return{
+          login: '',
+          signupIsClicked: false,
+          username: '',
+          password: ''
+        }
+    },
+    methods:{
+        signupFunc(){
+            this.signupIsClicked = !this.signupIsClicked
+            document.querySelector('.signin-card').style.height = '70%'
+            document.querySelector('.image').style.height = '100%'
+
+        },
+        signinFunc(){
+            this.signupIsClicked = !this.signupIsClicked
+            document.querySelector('.signin-card').style.height = '60%'
+            document.querySelector('.image').style.height = '90%'
+
+           
+
+        },
+        loginFunc(){
+            this.$emit('loginFunction',[this.username,this.password])
+        }
+    }
+}
+</script>
+
+<style>
+@import "./styles/global.css";
+</style>
+
+<style scoped>
+
+.main-content{
+    position: absolute;
+    height: 100vh;
+    width: 100vw;
+    overflow: hidden;
+}
+.main-content .signin-card{
+    position: absolute;
+    background: var(--bg-primary);
+    top: 5rem;
+    box-shadow: 0 0 1px #ccc;
+   width:50%;
+   height: 60%;
+    top: 50%;
+    left:50%;
+    transform: translate(-50%,-50%);
+}
+.signin-card .body{
+    position: relative;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+}
+.signin-card .body .image{
+    background-image: url('../assets/signin_image.png');
+    width: 50%;
+    height: 90%;
+    position: relative;
+background-size: cover;
+    background-position: center;
+    transition: .3s;
+}
+.signin-card .body .image:hover{
+    transform:scale(1.1);
+}
+.links a{
+    text-decoration: none;
+    color: var(--clr-secondary);
+}
+.links a:hover{
+    color: var(--clr-in-hover);
+}
+.form{
+    position: relative;
+    width: 100%;
+}
+.links a{
+    text-transform: uppercase;
+    font-weight: 600;
+}
+.circle-bg{
+    position: absolute;
+    top: 5%;
+    left: 10%;
+    width: 20%;
+    aspect-ratio: 10 / 9;
+    background: var(--circle-bg);
+    border-radius: 50%;
+    opacity: .5;
+    animation: animateWidth 1s ease forwards;
+}
+@keyframes animateWidth{
+    from{
+        width: 0;
+        left: 15%;
+    }
+    to{
+        width: 20%;
+    }
+}
+.circle-bg2{
+    position: absolute;
+    top: 9%;
+    right: -10%;
+    width: 40%;
+    aspect-ratio: 10 / 9;
+    background: var(--circle-bg);
+    border-radius: 50%;
+    animation: animateWidth2 1s ease forwards;
+
+}
+@keyframes animateWidth2{
+    from{
+        width: 0;
+    }
+    to{
+        width: 40%;
+    }
+}
+.welcome-back{
+    animation: animate1 1s ease forwards;
+    animation-delay: .1s;
+}
+@keyframes animate1{
+    from{
+        opacity: 0;
+    }
+    to{
+        opacity: 1;
+    }
+}
+</style>
